3ec9d5808e47ab1666ca0c8a47b0ea9f42982929
Dunabee <45811395+Dunabee@users.noreply.github.com>
2021-11-01 16:41:11 +0800

    Add files via upload
    update

--- thirdLogin.js
+++ thirdLogin.js
@@ -1,3 +1,8 @@
+/**
+ * 获取用户名   $.getNameInputValue()
+ * 获取密码     $.getPwdInputValue()
+ * 获取验证码   $.getCodeInputValue()
+ */
 (function($) {
     var loginSdk = 'baseLogin'.toLowerCase();
 
@@ -15,10 +20,12 @@
 
     $.fn.thirdLogin = function(options) {
         var defaults = {
-            sendMsg: function() {}
+            sendMsg: function() {},
+            register: function() {},
+            login: function() {},
+            changePwd: function() {},
         };
         var settings = $.extend(defaults, options);
-        settings.sendMsg()
 
         let html = '<div class="loginContainer">\
         <div class="loginHeader">登录 </div>\
@@ -45,7 +52,7 @@
                         </div>\
                     </div>\
                     <div class="loginBtn">\
-                        <div class="btn" data-smsloginreg="loginBtn">登录</div>\
+                        <div class="btn" data-baselogin="loginBtn">登录</div>\
                     </div>\
                 </div>\
                 <!-- 短信登录 -->\
@@ -57,15 +64,16 @@
                     </div>\
                     <div class="loginInfo">\
                         <div class="loginInfoItem">\
+                            <div class="dn infoError" data-smslogin="nameErr">手机号错误</div>\
                             <div class="loginAccount"><input data-smslogin="nameInput" class="inputBox" type="text" placeholder="请输入手机号" maxlength="11"></div>\
                         </div>\
                         <div class="loginInfoItem">\
                             <div class="loginAccount"><input data-smslogin="codeInput" class="inputBox" type="text" placeholder="请输入验证码" maxlength="11"></div>\
-                            <div class="verificationCode">获取验证码</div>\
+                            <div class="verificationCode" data-smslogin="getCode">获取验证码</div>\
                         </div>\
                     </div>\
                     <div class="loginBtn">\
-                        <div class="btn" data-smsloginreg="loginBtn">登录</div>\
+                        <div class="btn" data-smslogin="loginBtn">登录</div>\
                     </div>\
                 </div>\
                 <!-- 二维码登录 -->\
@@ -80,15 +88,16 @@
                     </div>\
                     <div class="loginInfo">\
                         <div class="loginInfoItem">\
+                            <div class="dn infoError" data-register="nameErr">手机号错误</div>\
                             <div class="loginAccount"><input data-register="nameInput" class="inputBox" type="text" placeholder="请输入手机号" maxlength="11"></div>\
                         </div>\
                         <div class="loginInfoItem">\
                             <div class="loginAccount"><input data-register="codeInput" class="inputBox" type="text" placeholder="请输入验证码" maxlength="11"></div>\
-                            <div class="verificationCode">获取验证码</div>\
+                            <div class="verificationCode" data-register="getCode">获取验证码</div>\
                         </div>\
                     </div>\
                     <div class="loginBtn">\
-                        <div class="btn">激活</div>\
+                        <div class="btn" data-register="registerBtn">激活</div>\
                     </div>\
                 </div>\
                 <!-- 忘记密码 -->\
@@ -99,136 +108,115 @@
                     </div>\
                     <div class="loginInfo">\
                         <div class="loginInfoItem">\
+                            <div class="dn infoError" data-changepwd="nameErr">手机号错误</div>\
                             <div class="loginAccount"><input data-changepwd="nameInput" class="inputBox" type="text" placeholder="请输入手机号" maxlength="11"></div>\
                         </div>\
                         <div class="loginInfoItem">\
                             <div class="loginAccount"><input data-changepwd="codeInput" class="inputBox" type="text" placeholder="请输入验证码" maxlength="11"></div>\
-                            <div class="verificationCode">获取验证码</div>\
+                            <div class="verificationCode" data-changepwd="getCode">获取验证码</div>\
                         </div>\
                         <div class="loginInfoItem">\
                             <div class="loginAccount"><input data-changepwd="pwdInput" class="inputBox" type="password" placeholder="请输入新的密码" maxlength="11"></div>\
                         </div>\
                     </div>\
                     <div class="loginBtn">\
-                        <div class="btn">确认修改</div>\
+                        <div class="btn" data-changepwd="changeBtn">确认修改</div>\
                     </div>\
                 </div>\
             </div>\
         </div>\
     </div>';
-        // html += '<div class="loginContainer">'
-        // html += '   <div class="loginHeader">登录';
-        // html += '   </div>';
-
-        // html += '   <div class="loginForm">';
-        // html += '       <div class="loginFormInner">';
-
-        // html += '           <div class="" data-login-sdk="baseLogin">';
-        // html += '               <div class="loginTab">';
-        // html += '                   <a href="javascript:;" class="selected">密码登录</a>';
-        // html += '                   <a href="javascript:;" data-baselogin="smsLogin">短信登录</a>';
-        // html += '               </div>密码登录';
-        // html += '               <div class="">';
-        // html += '                   <div>';
-        // html += '               </div>';
-        // html += '           </div>';
-
-        // html += '           <div class="dn" data-login-sdk="smsLogin">';
-        // html += '               <div class="loginTab">';
-        // html += '                   <a href="javascript:;" data-smslogin="baseLogin">密码登录</a>';
-        // html += '                   <a href="javascript:;" class="selected">短信登录</a>';
-        // html += '               </div>短信登录';
-        // html += '           </div>';
-
-        // html += '           <div class="dn" data-login-sdk="wechatLogin">wechatLogin</div>';
-
-        // html += '       </div>';
-        // html += '   </div>';
-
-        // html += '';
-        // html += '</div>'
         this.html(html);
 
         let that = this;
+        init(that);
         // 切换登录方式
         // 短信
         this.find('[data-baselogin="smsLogin"]')
             .on("click", function() {
                 loginSdk = 'smsLogin'.toLowerCase()
                 that.find('div[data-login-sdk="smsLogin"]').removeClass('dn').siblings().addClass('dn')
+                init(that);
             });
         // 密码登录
         this.find('[data-smslogin="baseLogin"]')
             .on("click", function() {
-                loginSdk = 'baseLogin'.toLowerCase()
+                loginSdk = 'baseLogin'.toLowerCase().toString()
                 that.find('div[data-login-sdk="baseLogin"]').removeClass('dn').siblings().addClass('dn')
+                init(that);
             });
         // 注册
         this.find('[data-baselogin="reg"]')
             .on("click", function() {
                 loginSdk = 'register'.toLowerCase()
                 that.find('div[data-login-sdk="register"]').removeClass('dn').siblings().addClass('dn')
+                init(that);
             });
         // 修改密码
         this.find('[data-baselogin="forgotPwd"]')
             .on("click", function() {
-                loginSdk = 'forgotPwd'.toLowerCase()
+                loginSdk = 'changePwd'.toLowerCase()
                 that.find('div[data-login-sdk="changePwd"]').removeClass('dn').siblings().addClass('dn')
+                init(that);
             });
         // WX
-        this.find('[data-baselogin="wechatLogin"]')
-            .on("click", function() {
-                loginSdk = ''.toLowerCase()
-                that.find('div[data-login-sdk="wechatLogin"]').removeClass('dn').siblings().addClass('dn')
-            });
-        this.find('[data-smslogin="wechatLogin"]')
+        this.find('[data-baselogin="wechatLogin"],[data-smslogin="wechatLogin"]')
             .on("click", function() {
                 loginSdk = ''.toLowerCase()
                 that.find('div[data-login-sdk="wechatLogin"]').removeClass('dn').siblings().addClass('dn')
             });
         this.find('[data-wechatlogin="baseLogin"]')
             .on("click", function() {
+                loginSdk = 'baseLogin'.toLowerCase()
                 that.find('div[data-login-sdk="baseLogin"]').removeClass('dn').siblings().addClass('dn')
+                init(that);
             });
         // 返回密码登录
-        this.find('[data-register="back"]')
+        this.find('[data-register="back"],[data-changePwd="back"]')
             .on("click", function() {
-                loginSdk = 'smsLogin'.toLowerCase()
+                loginSdk = 'baseLogin'.toLowerCase()
                 that.find('div[data-login-sdk="baseLogin"]').removeClass('dn').siblings().addClass('dn')
+                init(that);
             });
-        // 返回密码登录
-        this.find('[data-changePwd="back"]')
-            .on("click", function() {
-                loginSdk = 'smsLogin'.toLowerCase()
-                that.find('div[data-login-sdk="baseLogin"]').removeClass('dn').siblings().addClass('dn')
-            });
-        // 验证手机号
-        this.find('[data-baselogin="nameInput"]')
-            .focus(function() {
-                that.find('[data-baselogin="nameErr"]').addClass('dn')
-            })
-            .blur(function() {
-                let account = that.find('[data-baselogin="nameInput"]').val()
-                if (account.length != 11) {
-                    that.find('[data-baselogin="nameErr"]').removeClass('dn')
-                }
-            })
 
-        this.find('[data-smsloginreg="loginBtn"]')
+        // 发送验证码
+        this.find('[data-smslogin="getCode"],[data-register="getCode"],[data-changepwd="getCode"]')
             .on("click", function() {
-                console.log($.getNameInputValue())
-                console.log($.getCodeInputValue())
-                console.log($.getPwdInputValue())
+                settings.sendMsg()
             });
+        // 注册
+        this.find('[data-register="registerBtn"]')
+            .on("click", function() {
+                settings.register()
+            });
+        // 登录
+        this.find('[data-baselogin="loginBtn"],[data-smslogin="loginBtn"]')
+            .on("click", function() {
+                settings.login()
+            });
+        // 修改密码
+        this.find('[data-changepwd="changeBtn"]')
+            .on("click", function() {
+                settings.changePwd()
+            });
+
         return this;
     };
+
+    function verifyPhone(that) {
+        that.find(`[data-${loginSdk}="nameInput"]`)
+            .focus(function() {
+                that.find(`[data-${loginSdk}="nameErr"]`).addClass('dn')
+            })
+            .blur(function() {
+                let account = that.find(`[data-${loginSdk}="nameInput"]`).val()
+                if (account.length != 11) {
+                    that.find(`[data-${loginSdk}="nameErr"]`).removeClass('dn')
+                }
+            })
+    }
+
+    function init(that) {
+        verifyPhone(that)
+    }
 })(jQuery);
-
-
-
-function changeType(that, clickItem, showItem) {
-    that.find(`[${clickItem}]`)
-        .on("click", function() {
-            that.find(`div[${showItem}]`).removeClass('dn').siblings().addClass('dn')
-        });
-}
