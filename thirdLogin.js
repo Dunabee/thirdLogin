4206d4c6c176f57c62ed662f1b071600565ea187
Dunabee <45811395+Dunabee@users.noreply.github.com>
2021-11-19 18:01:38 +0800

    Add files via upload

--- thirdLogin.js
+++ thirdLogin.js
@@ -18,144 +18,45 @@
         }
     });
 
-    $.fn.thirdLogin = function(options) {
+    $.fn.thirdLogin_loginByPwd = function(options) {
         var defaults = {
-            onload: function() {},
-            sendMsg: function(argu) {},
-            register: function() {},
-            loginByPhone: function() {},
             loginByPwd: function() {},
-            changePwd: function() {},
         };
         var settings = $.extend(defaults, options);
-
-        let html = '<div class="loginContainer">\
-        <div class="loginHeader">登录 </div>\
-        <div class="loginForm">\
-            <div class="loginFormInner">\
-                <!-- 密码登录 -->\
-                <div class="" data-login-sdk="baseLogin">\
-                    <div class="imgContent" data-smslogin="wechatLogin"><img src="wxLogin.png"></div>\
-                    <div class="loginTab">\
-                        <a href="javascript:;" class="selected">密码登录</a>\
-                        <a href="javascript:;" data-baselogin="smsLogin">短信登录</a>\
-                    </div>\
-                    <div class="loginInfo">\
-                        <div class="loginInfoItem">\
-                            <div class="dn infoError" data-baselogin="nameErr">手机号错误</div>\
-                            <div class="loginAccount"><input data-baselogin="nameInput" class="inputBox" type="text" placeholder="请输入手机号" maxlength="11"></div>\
-                        </div>\
-                        <div class="loginInfoItem">\
-                            <div class="loginAccount"><input data-baselogin="pwdInput" class="inputBox" type="password" placeholder="请输入密码" maxlength="11"></div>\
-                        </div>\
-                        <div class="loginInfoItem">\
-                            <div class="loginAccount"><input data-baselogin="codeInput" class="inputBox" type="text" placeholder="验证码"></div>\
-                            <div class="imgCode" data-baselogin="getCode"><img id="imgSignCode" src=""></div>\
-                        </div>\
-                        <div class="loginInfoOthers">\
-                            <div class="register"><a data-baselogin="reg">激活账号</a></div>\
-                            <div class="forgotPwd"><a data-baselogin="forgotPwd">忘记密码</a></div>\
-                        </div>\
-                    </div>\
-                    <div class="loginBtn">\
-                        <div class="btn" data-baselogin="loginBtn">登录</div>\
-                    </div>\
+        let html = '<div class="" data-login-sdk="baseLogin">\
+            <div class="imgContent" data-smslogin="wechatLogin"><img src="../STYLE/images/wxLogin.png"></div>\
+            <div class="loginTab">\
+                <a href="javascript:;" class="selected">密码登录</a>\
+                <a href="javascript:;" data-baselogin="smsLogin">短信登录</a>\
+            </div>\
+            <div class="loginInfo">\
+                <div class="loginInfoItem">\
+                    <div class="loginAccount"><input data-baselogin="nameInput" class="inputBox" type="text" placeholder="请输入账号"></div>\
                 </div>\
-                <!-- 短信登录 -->\
-                <div class="dn" data-login-sdk="smsLogin">\
-                    <div class="imgContent" data-smslogin="wechatLogin"><img src="wxLogin.png"></div>\
-                    <div class="loginTab">\
-                        <a href="javascript:;" data-smslogin="baseLogin">密码登录</a>\
-                        <a href="javascript:;" class="selected">短信登录</a>\
-                    </div>\
-                    <div class="loginInfo">\
-                        <div class="loginInfoItem">\
-                            <div class="dn infoError" data-smslogin="nameErr">手机号错误</div>\
-                            <div class="loginAccount"><input data-smslogin="nameInput" class="inputBox" type="text" placeholder="请输入手机号" maxlength="11"></div>\
-                        </div>\
-                        <div class="loginInfoItem">\
-                            <div class="loginAccount"><input data-smslogin="codeInput" class="inputBox" type="text" placeholder="请输入验证码" maxlength="11"></div>\
-                            <div class="verificationCode" data-smslogin="getCode">获取验证码</div>\
-                        </div>\
-                    </div>\
-                    <div class="loginBtn">\
-                        <div class="btn" data-smslogin="loginBtn">登录</div>\
-                    </div>\
+                <div class="loginInfoItem">\
+                    <div class="loginAccount"><input data-baselogin="pwdInput" class="inputBox" type="password" placeholder="请输入密码"></div>\
                 </div>\
-                <!-- 二维码登录 -->\
-                <div class="dn" data-login-sdk="wechatLogin">\
-                    <div class="imgContent" data-wechatlogin="baseLogin"><img src="baseLogin.png"></div>\
-                    <div class="loginTitle">\
-                        <div><span>微信扫码登录</span></div>\
-                    </div>\
-                    <div class="loginInfo">\
-                        <div class="wxQRCode"><img id="imgWxCode" src=""/></div>\
-                    </div>\
+                <div class="loginInfoItem">\
+                    <div class="loginAccount"><input data-baselogin="codeInput" class="inputBox" type="text" placeholder="验证码"></div>\
+                    <div class="imgCode" data-baselogin="getCode"><img id="imgSignCode" src=""></div>\
                 </div>\
-                <!-- 注册 -->\
-                <div class="dn" data-login-sdk="register">\
-                    <div class="loginTitle">\
-                        <div><i data-register="back">返回</i></div>\
-                        <div><span>激活账号</span></div>\
-                    </div>\
-                    <div class="loginInfo">\
-                        <div class="loginInfoItem">\
-                            <div class="dn infoError" data-register="nameErr">手机号错误</div>\
-                            <div class="loginAccount"><input data-register="nameInput" class="inputBox" type="text" placeholder="请输入手机号" maxlength="11"></div>\
-                        </div>\
-                        <div class="loginInfoItem">\
-                            <div class="loginAccount"><input data-register="codeInput" class="inputBox" type="text" placeholder="请输入验证码" maxlength="11"></div>\
-                            <div class="verificationCode" data-register="getCode">获取验证码</div>\
-                        </div>\
-                    </div>\
-                    <div class="loginBtn">\
-                        <div class="btn" data-register="registerBtn">激活</div>\
-                    </div>\
-                </div>\
-                <!-- 忘记密码 -->\
-                <div class="dn" data-login-sdk="changePwd">\
-                    <div class="loginTitle">\
-                        <div><i data-changePwd="back">返回</i></div>\
-                        <div><span>修改密码</span></div>\
-                    </div>\
-                    <div class="loginInfo">\
-                        <div class="loginInfoItem">\
-                            <div class="dn infoError" data-changepwd="nameErr">手机号错误</div>\
-                            <div class="loginAccount"><input data-changepwd="nameInput" class="inputBox" type="text" placeholder="请输入手机号" maxlength="11"></div>\
-                        </div>\
-                        <div class="loginInfoItem">\
-                            <div class="loginAccount"><input data-changepwd="codeInput" class="inputBox" type="text" placeholder="请输入验证码" maxlength="11"></div>\
-                            <div class="verificationCode" data-changepwd="getCode">获取验证码</div>\
-                        </div>\
-                        <div class="loginInfoItem">\
-                            <div class="loginAccount"><input data-changepwd="pwdInput" class="inputBox" type="password" placeholder="请输入新的密码" maxlength="11"></div>\
-                        </div>\
-                    </div>\
-                    <div class="loginBtn">\
-                        <div class="btn" data-changepwd="changeBtn">确认修改</div>\
-                    </div>\
+                <div class="loginInfoOthers">\
+                    <div class="register"><a data-baselogin="reg">激活账号</a></div>\
+                    <div class="forgotPwd"><a data-baselogin="forgotPwd">忘记密码</a></div>\
                 </div>\
             </div>\
-        </div>\
-    </div>';
-        this.html(html);
-        settings.onload();
+            <div class="loginBtn">\
+                <div class="btn" data-baselogin="loginBtn">登录</div>\
+            </div>\
+        </div>'
+        this.append(html)
 
-        let that = this;
-        init(that);
-        // 切换登录方式
+        var that = this;
         // 短信
         this.find('[data-baselogin="smsLogin"]')
             .on("click", function() {
                 loginSdk = 'smsLogin'.toLowerCase()
                 that.find('div[data-login-sdk="smsLogin"]').removeClass('dn').siblings().addClass('dn')
-                init(that);
-            });
-        // 密码登录
-        this.find('[data-smslogin="baseLogin"]')
-            .on("click", function() {
-                loginSdk = 'baseLogin'.toLowerCase().toString()
-                that.find('div[data-login-sdk="baseLogin"]').removeClass('dn').siblings().addClass('dn')
                 init(that);
             });
         // 注册
@@ -173,27 +74,135 @@
                 init(that);
             });
         // WX
-        this.find('[data-baselogin="wechatLogin"],[data-smslogin="wechatLogin"]')
+        this.find('[data-baselogin="wechatLogin"]')
             .on("click", function() {
                 loginSdk = ''.toLowerCase()
                 that.find('div[data-login-sdk="wechatLogin"]').removeClass('dn').siblings().addClass('dn')
             });
+        // 登录
+        this.find('[data-baselogin="loginBtn"]')
+            .on("click", function() {
+                settings.loginByPwd()
+            });
+        return this
+    }
+
+    $.fn.thirdLogin_loginByMsg = function(options) {
+        var defaults = {
+            sendMsg: function(argu) {},
+            loginByPhone: function() {},
+        };
+        var settings = $.extend(defaults, options);
+        let html = '<div class="dn" data-login-sdk="smsLogin">\
+            <div class="imgContent" data-smslogin="wechatLogin"><img src="../STYLE/images/wxLogin.png"></div>\
+            <div class="loginTab">\
+                <a href="javascript:;" data-smslogin="baseLogin">密码登录</a>\
+                <a href="javascript:;" class="selected">短信登录</a>\
+            </div>\
+            <div class="loginInfo">\
+                <div class="loginInfoItem">\
+                    <div class="dn infoError" data-smslogin="nameErr">手机号错误</div>\
+                    <div class="loginAccount"><input data-smslogin="nameInput" class="inputBox" type="text" placeholder="请输入手机号" maxlength="11"></div>\
+                </div>\
+                <div class="loginInfoItem">\
+                    <div class="loginAccount"><input data-smslogin="codeInput" class="inputBox" type="text" placeholder="请输入验证码" maxlength="6"></div>\
+                    <div class="verificationCode" data-smslogin="getCode">获取验证码</div>\
+                </div>\
+            </div>\
+            <div class="loginBtn">\
+                <div class="btn" data-smslogin="loginBtn">登录</div>\
+            </div>\
+        </div>'
+        this.append(html)
+
+        var that = this;
+        // 密码登录
+        this.find('[data-smslogin="baseLogin"]')
+            .on("click", function() {
+                loginSdk = 'baseLogin'.toLowerCase().toString()
+                that.find('div[data-login-sdk="baseLogin"]').removeClass('dn').siblings().addClass('dn')
+                init(that);
+            });
+        this.find('[data-smslogin="wechatLogin"]')
+            .on("click", function() {
+                loginSdk = ''.toLowerCase()
+                that.find('div[data-login-sdk="wechatLogin"]').removeClass('dn').siblings().addClass('dn')
+            });
+        // 发送验证码
+        this.find('[data-smslogin="getCode"]')
+            .one("click", function() {
+                var argu = arguments.callee;
+                settings.sendMsg.call(this, argu)
+            });
+        // 登录
+        this.find('[data-smslogin="loginBtn"]')
+            .on("click", function() {
+                settings.loginByPhone()
+            });
+        return this
+    }
+
+    $.fn.thirdLogin_loginByWx = function() {
+        var defaults = {};
+        var settings = $.extend(defaults, options);
+        let html = '<div class="dn" data-login-sdk="wechatLogin">\
+            <div class="imgContent" data-wechatlogin="baseLogin"><img src="../STYLE/images/baseLogin.png"></div>\
+            <div class="loginTitle">\
+                <div><span>微信扫码登录</span></div>\
+            </div>\
+            <div class="loginInfo">\
+                <div class="wxQRCode"><img id="imgWxCode" src=""/></div>\
+            </div>\
+        </div>'
+        this.append(html)
+
+        var that = this;
         this.find('[data-wechatlogin="baseLogin"]')
             .on("click", function() {
                 loginSdk = 'baseLogin'.toLowerCase()
                 that.find('div[data-login-sdk="baseLogin"]').removeClass('dn').siblings().addClass('dn')
                 init(that);
             });
+        return this
+    }
+
+    $.fn.thirdLogin_register = function() {
+        var defaults = {
+            sendMsg: function(argu) {},
+            register: function() {},
+        };
+        var settings = $.extend(defaults, options);
+        let html = '<div class="dn" data-login-sdk="register">\
+            <div class="loginTitle">\
+                <div><i data-register="back">返回</i></div>\
+                <div><span>激活账号</span></div>\
+            </div>\
+            <div class="loginInfo">\
+                <div class="loginInfoItem">\
+                    <div class="dn infoError" data-register="nameErr">手机号错误</div>\
+                    <div class="loginAccount"><input data-register="nameInput" class="inputBox" type="text" placeholder="请输入手机号" maxlength="11"></div>\
+                </div>\
+                <div class="loginInfoItem">\
+                    <div class="loginAccount"><input data-register="codeInput" class="inputBox" type="text" placeholder="请输入验证码" maxlength="6"></div>\
+                    <div class="verificationCode" data-register="getCode">获取验证码</div>\
+                </div>\
+            </div>\
+            <div class="loginBtn">\
+                <div class="btn" data-register="registerBtn">激活</div>\
+            </div>\
+        </div>'
+        this.append(html)
+
+        var that = this;
         // 返回密码登录
-        this.find('[data-register="back"],[data-changePwd="back"]')
+        this.find('[data-register="back"]')
             .on("click", function() {
                 loginSdk = 'baseLogin'.toLowerCase()
                 that.find('div[data-login-sdk="baseLogin"]').removeClass('dn').siblings().addClass('dn')
                 init(that);
             });
-
         // 发送验证码
-        this.find('[data-smslogin="getCode"],[data-register="getCode"],[data-changepwd="getCode"]')
+        this.find('[data-register="getCode"]')
             .one("click", function() {
                 var argu = arguments.callee;
                 settings.sendMsg.call(this, argu)
@@ -203,25 +212,123 @@
             .on("click", function() {
                 settings.register()
             });
-        // 登录
-        this.find('[data-smslogin="loginBtn"]')
+        return this
+    }
+
+    $.fn.thirdLogin_forgotPwd = function() {
+        var defaults = {
+            sendMsg: function(argu) {},
+            changePwd: function() {},
+        };
+        var settings = $.extend(defaults, options);
+        let html = '<div class="dn" data-login-sdk="changePwd">\
+            <div class="loginTitle">\
+                <div><i data-changePwd="back">返回</i></div>\
+                <div><span>修改密码</span></div>\
+            </div>\
+            <div class="loginInfo">\
+                <div class="loginInfoItem">\
+                    <div class="dn infoError" data-changepwd="nameErr">手机号错误</div>\
+                    <div class="loginAccount"><input data-changepwd="nameInput" class="inputBox" type="text" placeholder="请输入手机号" maxlength="11"></div>\
+                </div>\
+                <div class="loginInfoItem">\
+                    <div class="loginAccount"><input data-changepwd="codeInput" class="inputBox" type="text" placeholder="请输入验证码" maxlength="6"></div>\
+                    <div class="verificationCode" data-changepwd="getCode">获取验证码</div>\
+                </div>\
+                <div class="loginInfoItem">\
+                    <div class="loginAccount"><input data-changepwd="pwdInput" class="inputBox" type="password" placeholder="请输入新的密码"></div>\
+                </div>\
+            </div>\
+            <div class="loginBtn">\
+                <div class="btn" data-changepwd="changeBtn">确认修改</div>\
+            </div>\
+        </div>'
+        this.append(html)
+
+        var that = this;
+        // 返回密码登录
+        this.find('[data-changePwd="back"]')
             .on("click", function() {
-                settings.loginByPhone()
+                loginSdk = 'baseLogin'.toLowerCase()
+                that.find('div[data-login-sdk="baseLogin"]').removeClass('dn').siblings().addClass('dn')
+                init(that);
             });
-        this.find('[data-baselogin="loginBtn"]')
-            .on("click", function() {
-                settings.loginByPwd()
+        // 发送验证码
+        this.find('[data-changepwd="getCode"]')
+            .one("click", function() {
+                var argu = arguments.callee;
+                settings.sendMsg.call(this, argu)
             });
         // 修改密码
         this.find('[data-changepwd="changeBtn"]')
             .on("click", function() {
                 settings.changePwd()
             });
+        return this
+    }
+
+    $.fn.thirdLogin = function(options) {
+        var defaults = {
+            onload: function() {},
+            sendMsg: function(argu) {},
+            register: function() {},
+            loginByPhone: function() {},
+            loginByPwd: function() {},
+            changePwd: function() {},
+        };
+        var settings = $.extend(defaults, options);
+
+        let html = '<div class="loginContainer">\
+            <div class="loginHeader">登录 </div>\
+            <div class="loginForm">\
+                <div class="loginFormInner">\
+                </div>\
+            </div>\
+        </div>';
+        this.html(html);
+
+        // 密码登录
+        $('.loginFormInner').thirdLogin_loginByPwd()
+
+        // 短信登录
+        $('.loginFormInner').thirdLogin_loginByMsg()
+
+        // 二维码登录
+        $('.loginFormInner').thirdLogin_loginByWx()
+
+        // 注册
+        $('.loginFormInner').thirdLogin_register()
+
+        // 忘记密码
+        $('.loginFormInner').thirdLogin_forgotPwd()
+
+        settings.onload();
+
+        let that = this;
+        init(that);
+
 
         this.find('#imgSignCode,#imgWxCode')
             .on("click", function() {
                 settings.onload()
             });
+
+        //回车登录事件
+        document.onkeydown = function(event) {
+            var e = event || window.event;
+            if (e && e.keyCode == 13) { //回车键的键值为13
+                switch (loginSdk) {
+                    case 'baselogin':
+                        settings.loginByPwd();
+                        break;
+                    case 'smslogin':
+                        settings.loginByPhone();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
 
         return this;
     };
